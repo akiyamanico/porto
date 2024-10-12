f3055671d09c16a8a574bddd3f805293c266f785
Fi Zilalil Huda <76860149+akiyamanico@users.noreply.github.com>
2024-10-12 12:51:14 +0700

    my portofolio

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Manrope', 'sans-serif'], // Manrope as the default sans font
+      },
+    },
+  },
+  plugins: [],
+};
